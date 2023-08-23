8ef784bdf762c1bdcdbd945aaf9e1051558f71aa
ppabis <piotr@pabis.eu>
2023-08-23 22:16:46 +0200

    Added components to recipe

--- recipe.tf
+++ recipe.tf
@@ -1,5 +1,6 @@
 resource "aws_imagebuilder_image_recipe" "my-image-recipe" {
-  // component { } // Will be added later 
+  component { component_arn = aws_imagebuilder_component.docker-component.arn }
+  component { component_arn = aws_imagebuilder_component.nginx-component.arn }
   name         = "my-image-recipe"
   parent_image = "arn:aws:imagebuilder:eu-central-1:aws:image/amazon-linux-2023-arm64/2023.x.x"
   version      = "1.0.0"
