b89caa5eebf087114fb04860b03ec208e790af67
ppabis <piotr@pabis.eu>
2023-08-24 20:58:49 +0200

    add parameters to the recipe

--- recipe.tf
+++ recipe.tf
@@ -1,6 +1,17 @@
 resource "aws_imagebuilder_image_recipe" "my-image-recipe" {
   component { component_arn = aws_imagebuilder_component.docker-component.arn }
-  component { component_arn = aws_imagebuilder_component.nginx-component.arn }
+  component { 
+    component_arn = aws_imagebuilder_component.nginx-component.arn
+    parameter {
+      name  = "port"
+      value = "9000"
+    }
+
+    parameter {
+      name = "proxy_or_cgi"
+      value = "cgi"
+    }
+  }
   name         = "my-image-recipe"
   parent_image = "arn:aws:imagebuilder:eu-central-1:aws:image/amazon-linux-2023-arm64/2023.x.x"
   version      = "1.0.0"
